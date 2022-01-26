0fdbee1e5d717da1d15a25d69077a4ad37c78378
JeUnsong <zzangjh5@gmail.com>
2022-01-26 16:06:08 +0900

    메소드 호출 / 리턴 / 리턴값 출력

---
+++ Hello/src/Hello.java
@@ -0,0 +1,17 @@
+
+public class Hello {
+	
+	public static int sum(int n,int m) {
+		return n+m;
+	}
+
+	public static void main(String[] args) {
+		int s;
+		s=sum(10,20);
+		System.out.println(sum(10,20));
+		System.out.println(s);
+		
+
+	}
+
+}
---
+++ Hello/src/Hello4.java
@@ -0,0 +1,30 @@
+
+public class Hello4 {
+	
+	public static int sum(int n, int m) {
+		return n + m;
+	}
+
+	public static void main(String[] args) {
+		int i = 20;
+		int s;
+		char a;
+		
+		s= sum(i,10);
+		a='?';
+		System.out.println(a);
+		System.out.println("Hello4");
+		System.out.println(s);
+		
+		
+
+		
+		
+		
+		//메소드 호출
+		//문자?출력
+		//헬로 문자열 출력
+		//정수 S 값 30출력
+	}
+
+}
