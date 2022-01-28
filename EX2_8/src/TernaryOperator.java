d7d5bf933511c4ff36eb713ee0097d688ecf86b8
JeUnsong <zzangjh5@gmail.com>
2022-01-28 17:55:15 +0900

    메타 데이터

--- EX2_8/src/TernaryOperator.java
+++ EX2_8/src/TernaryOperator.java
@@ -2,9 +2,12 @@
 public class TernaryOperator {
 
 	public static void main(String[] args) {
-		int a = 3, b = 5;
+
+		int a = 5, b = 3;
 		
 		System.out.println("두 수의 차는 " + ((a>b)?(a-b):(b-a)));
+		
+		System.out.println((a>b)?(a-b):(b-a));
 
 	}
 
