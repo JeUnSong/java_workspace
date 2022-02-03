a8d1c8d4fe79abba4bd8784122e40b3f956057cd
JeUnsong <zzangjh5@gmail.com>
2022-02-03 19:33:21 +0900

    메타 데이터

--- Assignment/src/Assignment4_4.java
+++ Assignment/src/Assignment4_4.java
@@ -2,11 +2,15 @@
 public class Assignment4_4 {
 
 	public static void main(String[] args) {
-		for(int i=1; i<=5; i++) {
-			for(int j=5; j<=5; j++) {
-				System.out.println('*');
+		
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5-i; j++) {
+				System.out.print("*");
 			}
+			System.out.println();
 		}
+		
 	}
-
 }
+
+
