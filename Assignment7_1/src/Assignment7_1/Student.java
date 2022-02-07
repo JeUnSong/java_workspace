ff96c99d1d53fe74fdea9e9c1b7d4fbc001c5670
JeUnsong <zzangjh5@gmail.com>
2022-02-07 18:06:00 +0900

    클래서의 scanner를 사용하여 Student 배열을 a란들어 이름과 점수 입력하고 출력하기

---
+++ Assignment7_1/src/Assignment7_1/Student.java
@@ -0,0 +1,13 @@
+package Assignment7_1;
+//1. 5명의 학생을 저장하는 Student 배열을 만들어서 이름과 점수를 입력하고 출력하기.
+//=> scanner사용
+//=> 연습문제6-1번의 Student클래스 사용
+public class Student {
+	String name;
+	double score;
+	
+	public Student(String name, double score) {
+	this.name = name;
+	this.score = score;
+	}
+}
---
+++ Assignment7_1/src/Assignment7_1/StudentApp.java
@@ -0,0 +1,27 @@
+package Assignment7_1;
+
+import java.util.Scanner;
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+		Student [] student = new Student[5];
+		
+		Scanner scanner = new Scanner(System.in);
+		for(int i=0; i<student.length; i++) {
+			System.out.print("이름 : ");
+			String name = scanner.next();
+			System.out.print("점수 : ");
+			int score = scanner.nextInt();
+			student[i] = new Student(name, score);
+		}
+		
+		for(int i=0; i<student.length; i++) {
+			System.out.print("이름 : " + student[i].name +" "+ "점수 : " + student[i].score + " ");
+			System.out.println();
+		}
+		scanner.close();
+
+	}
+
+}
