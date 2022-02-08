439b5813968683d43a9a55ea3cbaaa41b551498e
JeUnsong <zzangjh5@gmail.com>
2022-02-08 17:29:56 +0900

    메소드 오버라이딩 예

---
+++ EX5_06/src/Overridint.java
@@ -0,0 +1,13 @@
+
+public class Overridint {
+
+	public static void main(String[] args) {
+		Weapon weapon = new Weapon();
+		System.out.println("기본 무기의 살상 능력은 " + weapon.fire());
+		
+		weapon = new Cannon();
+		System.out.println("대포의 살상 능력은 " + weapon.fire());
+
+	}
+
+}
