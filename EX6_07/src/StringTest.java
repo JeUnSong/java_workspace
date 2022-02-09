d8918f34fcf01ba30bc3867ca16c540c7da83b0b
JeUnsong <zzangjh5@gmail.com>
2022-02-09 19:05:04 +0900

    String 클래스의 다양한 메소드 활용 연습

---
+++ EX6_07/src/StringTest.java
@@ -0,0 +1,34 @@
+
+public class StringTest {
+
+	public static void main(String[] args) {
+		String tel="010-3320-9105";
+		
+		System.out.println(tel.replace("-", "."));
+		System.out.println(tel); // 원본은 변화 없음
+		
+		System.out.println(tel.substring(0, 3));
+		
+		String[] result=tel.split("-");
+		for(String s:result) {
+			System.out.println(s);
+		}
+		
+		String csv="홍길동,컴퓨터공학과,서울시";
+		String[] result2=csv.split(",");
+		for(String s2:result2) {
+			System.out.println(s2);
+		}
+		
+		String fileName="report.html";
+		int position=fileName.indexOf(".");
+		System.out.println(position);
+		System.out.println(fileName.substring(position+1));
+		
+		
+		String movieFileName="2022.02.09.movie.mp4";
+		System.out.println(movieFileName.substring(movieFileName.indexOf(".")+1));
+		System.out.println(movieFileName.substring(movieFileName.lastIndexOf(".")+1));
+		
+	}
+}
