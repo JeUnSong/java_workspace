161b0575c772d108d97e960c546359b774cef6cf
JeUnsong <zzangjh5@gmail.com>
2022-02-16 13:09:17 +0900

    JPanel을 상속받아 도형 그리기

---
+++ EX12_01/src/paintJPanelEx.java
@@ -0,0 +1,37 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+
+
+public class paintJPanelEx extends JFrame {
+private MyPanel panel = new MyPanel();
+	
+	public paintJPanelEx() {
+		this.setTitle("fillXXX 사용 예제");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setContentPane(panel);
+		
+		this.setSize(250, 220);
+		this.setVisible(true);
+	}
+	
+	class MyPanel extends JPanel {
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			g.setColor(Color.BLUE);
+			g.drawRect(10, 10, 50, 50);
+			g.drawRect(50, 50, 50, 50);
+			g.setColor(Color.MAGENTA);
+			g.drawRect(90, 90, 50, 50);
+		}
+	}
+
+	public static void main(String[] args) {
+		new paintJPanelEx();
+
+	}
+}
+
