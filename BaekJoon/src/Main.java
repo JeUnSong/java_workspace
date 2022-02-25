116198574695eec099d6a72c04b14c446625f6b0
JeUnsong <zzangjh5@gmail.com>
2022-02-25 18:18:07 +0900

    메타

--- BaekJoon/src/Main.java
+++ BaekJoon/src/Main.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		String name = "송제운";
+		String address= "경기도 부천시";
+			
+			System.out.print(name+address);
+		
+			
         
 		
 		
