b44bff76ffe27bcf077810064457b81194bc044b
JeUnsong <zzangjh5@gmail.com>
2022-03-15 18:18:44 +0900

    로그인 페이지
    DB연동없이 IF문에 단순 조건(아이디 비밀번호) 로 코딩 되어있음

--- Haksa2/src/Login.java
+++ Haksa2/src/Login.java
@@ -5,18 +5,29 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Vector;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class Login extends JFrame{
-	JTextField tfid = null;
-	JTextField tfpw = null;
+	JTextField tfId = null;
+	JTextField tfPw = null;
 	JButton btnLogin = null;
 	JButton btnSign = null;
 
+DefaultTableModel model=null;
 JPanel panel2;
 	Login(){
 		
@@ -28,32 +39,37 @@ JPanel panel2;
 		  setIconImage(img);
 		  
 		  setLayout(null);
+
+		  this.tfId = new JTextField("아이디");
+		  this.tfId.setLocation(240,390);
+		  this.tfId.setSize(150,25);
+		  this.add(tfId);
 		  
-		  this.tfid = new JTextField("아이디");
-		  this.tfid.setLocation(240,390);
-		  this.tfid.setSize(150,25);
-		  this.add(tfid);
-		  
-		  this.tfpw = new JTextField("비밀번호");
-		  this.tfpw.setLocation(240,420);
-		  this.tfpw.setSize(150,25);
-		  this.add(tfpw);
+		  this.tfPw = new JTextField("비밀번호");
+		  this.tfPw.setLocation(240,420);
+		  this.tfPw.setSize(150,25);
+		  this.add(tfPw);
+		  //this.tfPw.setEchoChar('*');
 		  
 		  this.btnLogin = new JButton("로그인");
 		  this.btnLogin.setLocation(405,392);
 		  this.btnLogin.setSize(75,50);
 		  this.btnLogin.addActionListener(new ActionListener() {
 
+			//DB에 있는 아이디 비밀번호 정보를 가지고 IF 조건문에 넣어야함
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				dispose(); // 이전창 닫기
-				panel2.removeAll(); //모든컴포넌트 삭제
-				panel2.revalidate(); //다시 활성화
-				panel2.repaint();    //다시 그리기
-				panel2.add(new Haksa()); //화면 생성.
-				panel2.setLayout(null);//레이아웃적용안함
 				
-				
+				if(tfId.getText().equals("테스트") && tfPw.getText().equals("테스트")) {
+						dispose(); // 이전창 닫기
+						panel2.removeAll(); //모든컴포넌트 삭제
+						panel2.revalidate(); //다시 활성화
+						panel2.repaint();    //다시 그리기
+						panel2.add(new Haksa()); //화면 생성.
+						panel2.setLayout(null);//레이아웃적용안함
+				}else {
+					JOptionPane.showMessageDialog(null, "아이디와 비밀번호를 확인해 주세요.", "로그인 오류", JOptionPane.ERROR_MESSAGE);
+				}
 			}});
 		  this.add(btnLogin);
 		  
