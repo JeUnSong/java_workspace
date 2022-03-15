3649d71c1773cc33a32a6204e23d1fd20f07a233
JeUnsong <zzangjh5@gmail.com>
2022-03-15 18:18:06 +0900

    회원가입 페이지
    DB연동 완료 / 그외 상세한 기능 모두 구현 안됨

--- Haksa2/src/SignUp.java
+++ Haksa2/src/SignUp.java
@@ -1,31 +1,45 @@
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 public class SignUp extends JFrame {
 	
-	JTextField tfidna = null;
-	JTextField tfpw = null;
-	JTextField tfid = null;
-	JTextField tfname = null;
-	JTextField tfemail = null;
-	JTextField tfcernu = null;
+	JTextField tfIdna = null;
+	JTextField tfPw = null;
+	JTextField tfId = null;
+	JTextField tfName = null;
+	JTextField tfEmail = null;
+	JTextField tfCernu = null;
 	
 	JLabel title = null;
-	JLabel jlidna = null;
-	JLabel jlpw = null;
-	JLabel jlid = null;
-	JLabel jlname = null;
-	JLabel jlemail = null;
-	JLabel jlcernu = null;
+	JLabel jlIdna = null;
+	JLabel jlPw = null;
+	JLabel jlId = null;
+	JLabel jlName = null;
+	JLabel jlEmail = null;
+	JLabel jlCernu = null;
 	
 	JButton btnCertification = null;
 	JButton btnSummit = null;
+	
+	DefaultTableModel model=null;
+	
+	//정보 디비로 전송까지만 구현
+	//그외 아무것도 구현 안됨
+	//(이메일인증 / 가입완료창 팝업 후 메인화면으로 이동 / 뒤로가기 / 비밀번호 확인 / 등등)
+	
 	
 	SignUp(){
 		
@@ -38,94 +52,113 @@ public class SignUp extends JFrame {
 		this.title.setSize(150,25);
 		this.add(title);
 	    
-		this.jlidna = new JLabel("아이디 ");
-	    this.jlidna.setLocation(70,320);
-		this.jlidna.setSize(150,25);
-		this.add(jlidna);
+		this.jlIdna = new JLabel("아이디 ");
+	    this.jlIdna.setLocation(70,320);
+		this.jlIdna.setSize(150,25);
+		this.add(jlIdna);
 		
-		this.tfidna = new JTextField(15);
-		this.tfidna.setLocation(120,320);
-		this.tfidna.setSize(150,25);
-		this.add(tfidna);
+		this.tfIdna = new JTextField(15);
+		this.tfIdna.setLocation(120,320);
+		this.tfIdna.setSize(150,25);
+		this.add(tfIdna);
 		
-		this.jlpw = new JLabel("비밀번호 ");
-	    this.jlpw.setLocation(360,320);
-		this.jlpw.setSize(150,25);
-		this.add(jlpw);
+		this.jlPw = new JLabel("비밀번호 ");
+	    this.jlPw.setLocation(360,320);
+		this.jlPw.setSize(150,25);
+		this.add(jlPw);
 		
-		this.tfpw = new JTextField(15);
-		this.tfpw.setLocation(420,320);
-		this.tfpw.setSize(150,25);
-		this.add(tfpw);
+		this.tfPw = new JTextField(15);
+		this.tfPw.setLocation(420,320);
+		this.tfPw.setSize(150,25);
+		this.add(tfPw);
 		
-		this.jlid = new JLabel("학    번 ");
-	    this.jlid.setLocation(70,360);
-		this.jlid.setSize(150,25);
-		this.add(jlid);
+		this.jlId = new JLabel("학    번 ");
+	    this.jlId.setLocation(70,360);
+		this.jlId.setSize(150,25);
+		this.add(jlId);
 		
-		this.tfid = new JTextField(15);
-		this.tfid.setLocation(120,360);
-		this.tfid.setSize(150,25);
-		this.add(tfid);
+		this.tfId = new JTextField(15);
+		this.tfId.setLocation(120,360);
+		this.tfId.setSize(150,25);
+		this.add(tfId);
 		
-		this.jlname = new JLabel("이        름 ");
-	    this.jlname.setLocation(360,360);
-		this.jlname.setSize(150,25);
-		this.add(jlname);
+		this.jlName = new JLabel("이        름 ");
+	    this.jlName.setLocation(360,360);
+		this.jlName.setSize(150,25);
+		this.add(jlName);
 		
-		this.tfname = new JTextField(15);
-		this.tfname.setLocation(420,360);
-		this.tfname.setSize(150,25);
-		this.add(tfname);
+		this.tfName = new JTextField(15);
+		this.tfName.setLocation(420,360);
+		this.tfName.setSize(150,25);
+		this.add(tfName);
 		
-		this.jlemail = new JLabel("EMAIL ");
-	    this.jlemail.setLocation(70,400);
-		this.jlemail.setSize(150,25);
-		this.add(jlemail);
+		this.jlEmail = new JLabel("EMAIL ");
+	    this.jlEmail.setLocation(70,400);
+		this.jlEmail.setSize(150,25);
+		this.add(jlEmail);
 		
-		this.tfemail = new JTextField(100);
-		this.tfemail.setLocation(120,400);
-		this.tfemail.setSize(150,25);
-		this.add(tfemail);
+		this.tfEmail = new JTextField(100);
+		this.tfEmail.setLocation(120,400);
+		this.tfEmail.setSize(150,25);
+		this.add(tfEmail);
 		
-		this.btnCertification = new JButton("인증메일 받기");
+		this.btnCertification = new JButton("인증메일(미구현)");
 		this.btnCertification.setLocation(135,430);
 		this.btnCertification.setSize(120,25);
 	    this.add(btnCertification);
 		
-	    this.jlcernu = new JLabel("인증번호 ");
-	    this.jlcernu.setLocation(360,400);
-		this.jlcernu.setSize(150,25);
-		this.add(jlcernu);
+	    this.jlCernu = new JLabel("인증번호 ");
+	    this.jlCernu.setLocation(360,400);
+		this.jlCernu.setSize(150,25);
+		this.add(jlCernu);
 		
-		this.tfcernu = new JTextField(15);
-		this.tfcernu.setLocation(420,400);
-		this.tfcernu.setSize(150,25);
-		this.add(tfcernu);
+		this.tfCernu = new JTextField("미구현");
+		this.tfCernu.setLocation(420,400);
+		this.tfCernu.setSize(150,25);
+		this.add(tfCernu);
 		
 		this.btnSummit = new JButton("가입하기");
 		this.btnSummit.setLocation(270,480);
 		this.btnSummit.setSize(120,30);
+		this.btnSummit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Class.forName("oracle.jdbc.driver.OracleDriver");
+					Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","ora_user","hong");
+					System.out.println("연결완료");
+					
+					Statement stmt = conn.createStatement();
+		
+					stmt.executeUpdate("insert into member values('"+tfIdna.getText()+"','"+tfPw.getText()+"','"+tfId.getText()+"','"+tfName.getText()+"','"+tfEmail.getText()+"')");
+
+					ResultSet rs = stmt.executeQuery("select * from member");
+					
+					model.setRowCount(0); // 목록 초기화
+					while(rs.next()) {
+						String[] row=new String[5];
+						row[0]=rs.getString("tfIdna");
+						row[1]=rs.getString("tfPw");
+						row[2]=rs.getString("tfId");
+						row[3]=rs.getString("tfName");
+						row[4]=rs.getString("tfEmail");
+						model.addRow(row);
+					}
+					rs.close();
+					stmt.close();
+					conn.close();
+				}catch(Exception e1) {
+					e1.printStackTrace();
+				}
+				
+			}});
+				
 	    this.add(btnSummit);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 		this.setSize(700,600);
 		this.setVisible(true);
 		
+	
 	}
-
 }
